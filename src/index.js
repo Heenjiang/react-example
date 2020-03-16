df0eeff02cd545e3cbca3b8ab58bc1e6f6f238ec
Lucas <1207881522@qq.com>
2020-03-16 21:30:38 +0300

    2 nd day in React Tutorial

--- src/index.js
+++ src/index.js
@@ -2,153 +2,146 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Square(props){
-    return (
-        <button 
-            className="square" onClick={props.onClick}>
-            {props.value}
-        </button>
-    );
+
+// function formatName(user){
+//   return user.firstName + ' ' + user.lastName;
+// }
+
+// const user = {
+//   firstName : 'Harper',
+//   lastName : 'Perez'
+// }
+
+// const element = (
+//   <h1>
+//     Hello, {formatName(user)}!
+//   </h1>
+// );
+
+//   ReactDOM.render(
+//     element,
+//     document.getElementById('root')
+//   );
+  
+
+// function tick(){
+//   const element = (
+//     <div>
+//       <h1>Hello, World!</h1>
+//       <h1>It is {new Date().toLocaleTimeString()}.</h1>
+//     </div>
+//   );
+//   ReactDOM.render(element, document.getElementById('root'));
+// }
+
+// setInterval(tick, 1000)
+
+// function Welcom(props){
+//   return <h1>Hello, {props.name}</h1>
+// }
+
+// const element = <Welcom name='Sara' />
+
+// ReactDOM.render(
+//   element, 
+//   document.getElementById('root')
+// );
+
+// function Welcom(props){
+// return <h1>Hello, {props.name}</h1>
+// }
+
+// function App() {
+//   return (
+//     <div>
+//       <Welcom name='Sara' />
+//       <Welcom name='Nicole'/>
+//       <Welcom name='Hoen' />
+//     </div>
+//   );
+// }
+
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
+
+function formatDate(date){
+  return date.toLocaleDateString();
 }
 
-class Board extends React.Component {
-    renderSquare(i) {
-        return (
-          <Square
-            value={this.props.squares[i]}
-            onClick={() => this.props.onClick(i)}
-          />
-        );
-      }
-  
-    render() {
-      return (
-        <div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
-        </div>
-      );
-    }
-  }
-  
-class Game extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            history: [{
-              squares: Array(9).fill(null),
-            }],
-            xIsNext: true,
-            stepNumber : 0,
-          };
-        }
-    
-    handleClick (i){
-        //根据当前的stepNumber复制history数组（因为由回溯功能，相当于如果用户点了回到之前的一步，然后就会
-        //改变stepNbumber,从而导致history也会相对更新）
-        const history = this.state.history.slice(0, this.state.stepNumber + 1);
-        //从history里复制出当前棋局数组的状态
-        const current =  history[history.length - 1]
-        //复制当前棋局状态
-        const squares = current.squares.slice();
-        //当某个玩家已经胜出时，或者这个square已经被点击过了，就什么也不做
-        if (calculateWinner(squares) || squares[i]){
-            return;
-        }
-        //根据双方轮换顺序设置当前点击方块的符号
-        squares[i] = this.state.xIsNext ? 'X' : 'O'
-        //更新game的属性
-        this.setState({
-            history: history.concat([{
-                squares: squares,
-            }]),
-            xIsNext: !this.state.xIsNext,
-            stepNumber: history.length,
-        });
-    }   
+// function Comment(props) {
+//   return(
+//     <div className="Comment">
+//       <div className="UserInfo">
+//         <img
+//           className="Avatar"
+//           src={props.author.avatarUrl}
+//           alt={props.author.name}
+//         />
+//         <div calssName="UserInfo-name">
+//           {props.author.name}
+//         </div>
+//       </div>
+//       <div className="Comment-date">
+//         {props.text}
+//       </div>
+//       <div className="Comment-date">
+//         {formatDate(props.date)}
+//       </div>
+//     </div>
+//   );
+// }
 
-    jumpTo(step) {
-        this.setState({
-          stepNumber: step,
-          xIsNext: (step % 2) === 0,
-        });
-      }
-
-    render() {
-        const history = this.state.history;
-        const current = history[this.state.stepNumber];
-        const winner = calculateWinner(current.squares);
-        //step:是一个数组对象，move是数组的下标
-        const moves = history.map((step, move) => {
-            const desc = move ?
-              'Go to move #' + move :
-              'Go to game start';
-            return (
-              <li key={move}>
-                <button onClick={() => this.jumpTo(move)}>{desc}</button>
-              </li>
-            );
-          });
-        let status;
-        if(winner){
-            status = 'Winner is ' + winner;
-        }
-        else{
-            status = 'Next player is: ' + (this.state.xIsNext ? 'X' : 'O')
-        }
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board 
-                squares={current.squares}
-                onClick={(i) => this.handleClick(i)}
-            />
-          </div>
-          <div className="game-info">
-            <div>{status}</div>
-            <ol>{moves}</ol>
-          </div>
-        </div>
-      );
-    }
-  }
-  
-  ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
+//改写comment组件：
+function Avatar(props) {
+  return(
+    <img className="Avatar"
+      src={props.user.avatarUrl}
+      alt={props.user.name}
+    />
   );
-  
+}
 
-  function calculateWinner(squares){
-      //tic-tac-toe一共有八种赢的直线关系
-      const winLines =  [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-      ];
-      for(let i = 0; i < winLines.length; i++){
-        const [a, b, c] =  winLines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
-            return squares[a];
-        }
-      }
-      return null;
-  }
+function UserInfo(props) {
+  return(
+    <div className="UserInfo">
+      <Avatar user={props.user} />
+      <div className="UserInfo-name">
+        {props.user.name}
+      </div>
+    </div>
+  );
+}
+
+function Comment(props) {
+  return(
+    <div className="Comment">
+      <UserInfo user={props.author} />
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(comment.date)}
+      </div>
+    </div>
+  );
+}
+
+const comment = {
+  date : new Date(),
+  text : 'I hope you enjoy learning React!',
+  author : {
+    name : "Hoen",
+    avatarUrl : 'https://placekitten.com/g/64/64'
+  },
+};
+
+ReactDOM.render(
+  <Comment 
+    date={comment.date}
+    text={comment.text}
+    author={comment.author}
+  />,
+  document.getElementById('root')
+);
