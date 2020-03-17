4a86cd5b61e62ed4ce1dbfa1f1782d895be1dcc1
Lucas <1207881522@qq.com>
2020-03-17 21:28:33 +0300

    3rd day: event handler methods

--- src/index.js
+++ src/index.js
@@ -3,145 +3,69 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-// function formatName(user){
-//   return user.firstName + ' ' + user.lastName;
-// }
+class Toggle extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {isToggleOn: true};
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-// const user = {
-//   firstName : 'Harper',
-//   lastName : 'Perez'
-// }
+  handleClick() {
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn     
+    }));
+  }
 
-// const element = (
-//   <h1>
-//     Hello, {formatName(user)}!
-//   </h1>
-// );
-
-//   ReactDOM.render(
-//     element,
-//     document.getElementById('root')
-//   );
-  
-
-// function tick(){
-//   const element = (
-//     <div>
-//       <h1>Hello, World!</h1>
-//       <h1>It is {new Date().toLocaleTimeString()}.</h1>
-//     </div>
-//   );
-//   ReactDOM.render(element, document.getElementById('root'));
-// }
-
-// setInterval(tick, 1000)
-
-// function Welcom(props){
-//   return <h1>Hello, {props.name}</h1>
-// }
-
-// const element = <Welcom name='Sara' />
-
-// ReactDOM.render(
-//   element, 
-//   document.getElementById('root')
-// );
-
-// function Welcom(props){
-// return <h1>Hello, {props.name}</h1>
-// }
-
-// function App() {
-//   return (
-//     <div>
-//       <Welcom name='Sara' />
-//       <Welcom name='Nicole'/>
-//       <Welcom name='Hoen' />
-//     </div>
-//   );
-// }
-
-// ReactDOM.render(
-//   <App />,
-//   document.getElementById('root')
-// );
-
-function formatDate(date){
-  return date.toLocaleDateString();
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
+    );
+  }
 }
 
-// function Comment(props) {
-//   return(
-//     <div className="Comment">
-//       <div className="UserInfo">
-//         <img
-//           className="Avatar"
-//           src={props.author.avatarUrl}
-//           alt={props.author.name}
-//         />
-//         <div calssName="UserInfo-name">
-//           {props.author.name}
-//         </div>
-//       </div>
-//       <div className="Comment-date">
-//         {props.text}
-//       </div>
-//       <div className="Comment-date">
-//         {formatDate(props.date)}
-//       </div>
-//     </div>
-//   );
-// }
+class LoggingButton extends React.Component {
+  //此语法确认handleClick 中的‘this’已被绑定
+  //注意：这是 *实验性*语法
+  handleClick = () =>{
+    console.log('this is', this);
+  }
 
-//改写comment组件：
-function Avatar(props) {
-  return(
-    <img className="Avatar"
-      src={props.user.avatarUrl}
-      alt={props.user.name}
-    />
-  );
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        Click Me
+      </button>
+    );
+  }
 }
 
-function UserInfo(props) {
-  return(
-    <div className="UserInfo">
-      <Avatar user={props.user} />
-      <div className="UserInfo-name">
-        {props.user.name}
-      </div>
-    </div>
-  );
-}
+class LoggingButton2 extends React.Component {
+  handleClick() {
+    console.log('this is:', this)
+  }
 
-function Comment(props) {
-  return(
-    <div className="Comment">
-      <UserInfo user={props.author} />
-      <div className="Comment-text">
-        {props.text}
-      </div>
-      <div className="Comment-date">
-        {formatDate(comment.date)}
-      </div>
-    </div>
-  );
+  render() {
+    return (
+       //此语法确认handleClick 中的‘this’已被绑定:不建议！！
+      <button onClick={(e) => this.handleClick(e)}>
+        Click Me
+      </button>
+    );
+  }
 }
-
-const comment = {
-  date : new Date(),
-  text : 'I hope you enjoy learning React!',
-  author : {
-    name : "Hoen",
-    avatarUrl : 'https://placekitten.com/g/64/64'
-  },
-};
 
 ReactDOM.render(
-  <Comment 
-    date={comment.date}
-    text={comment.text}
-    author={comment.author}
-  />,
+  <div>
+    <Toggle />
+    <LoggingButton />
+    <LoggingButton2 />
+  </div>,
   document.getElementById('root')
 );
+
+//向事件处理函数传递参数：
+//<button onClick={(e) => this.deleteRow(id, e)}>Delete Row</button>
+//<button onClick={this.deleteRow(this, id)}>Delete Row</button>
+
