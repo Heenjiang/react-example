7d2eee9ac9e2d06a1ff577742c67bd54a0c9901d
Heenjiang <12007881522@qq.com>
2020-03-20 00:50:33 +0300

    状态提升

--- src/index.js
+++ src/index.js
@@ -2,154 +2,107 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-// function NumberList(props) {
-//   const numbers = props.numbers;
-//   const listItems = numbers.map((number) =>
-//     <li key={number.toString()}>
-//       {number}
-//     </li>
-//   );
-//   return (
-//     <ul>{listItems}</ul>
-//   );
-// }
+const scaleNames = {
+  c: 'Celsius',
+  f: 'Fahrenheit'
+};
 
-// const numbers = [1, 2, 3, 4, 5];
-// ReactDOM.render(
-//   <NumberList numbers={numbers} />,
-//   document.getElementById('root')
-// );
+function BoilingVerdict(props) {
+  if (props.celsius && props.celsius >= 100){
+    return <p>The water would boil</p>;
+  }
+  else if(props.celsius && props.celsius < 100){
+    return <p>The water would not boil</p>;
+  }
+  return null;;
+}
 
-// function Blog(props) {
-//   const sidebar = (
-//     <ul>
-//       {props.posts.map((post) => 
-//       <li key={post.id}>
-//         {post.title}
-//       </li>)}
-//     </ul>
-//   );
+function toCelsius(fahrenheit) {
+  return (fahrenheit - 32) * 5 / 9;
+}
 
-//   const content = props.posts.map((post) => 
-//       <div key={post.id}>
-//         <h3>{post.title}</h3>
-//       <p>{post.content}</p>
-//       </div>
-//   );
+function toFahrenheit(celsius) {
+  return (celsius * 9 / 5) + 32;
+}
 
-//   return (
-//     <div>
-//       {sidebar}
-//       <hr />
-//       {content}
-//     </div>
-//   );
-// }
+function tryConvert(temperature, convert) {
+  const input = parseFloat(temperature);
+  if (Number.isNaN(input)){
+    return '';
+  }
+  const output = convert(input);
+  const rounded = Math.round(output * 1000) / 1000;
+  return rounded.toString;
+}
 
-// const posts = [
-//   {id: 1, title: 'Hello World', content: 'Welcome to learning React!'},
-//   {id: 2, title: 'Installation', content: 'You can install React from npm.'}
-// ];
+class TemperatureInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state ={temperature: ''};
+  }
 
-// ReactDOM.render(
-//   <Blog posts={posts} />,
-//   document.getElementById('root')
-// );
+  handleChange(e) {
+    this.props.onTemperatureChange(e.target.value);
+  }
 
-/**
- * Form 
- */
+  render(){
+    const temperature = this.props.temperature;
+    const scale = this.props.scale;
+    return (
+      <fieldset>
+        <legend>Enter temperature in {scaleNames[scale]}</legend>
+        <input value={temperature}
+               onChange={this.handleChange} />
+      </fieldset>
+    );
+  }
+}
 
-//  class NameForm extends React.Component {
-//    constructor(props) {
-//      super(props);
-//      this.state = {value: ''};
+class Caculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
+    this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
+    this.state = {
+        temperature: '',
+        scale: ''
+    };
+  }
 
-//      this.hanleChange = this.hanleChange.bind(this);
-//      this.handleSubmit = this.handleSubmit.bind(this);
-//    }
+  handleCelsiusChange(temperature) {
+    this.setState({scale: 'c', temperature});
+  }
 
-//    hanleChange(event){
-//     this.setState({value: event.target.value});
-//    }
+  handleFahrenheitChange(temperature) {
+    this.setState({scale: 'f', temperature});
+  }
+  render() {
+    const scale = this.state.scale;
+    const temperature = this.state.temperature;
+    const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
+    const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
 
-//    handleSubmit(event) {
-//      alert('Submit name: ' + this.state.value);
-//      event.preventDefault();
-//    }
+    return (
+      <div>
+        <TemperatureInput 
+          scale="c"
+          temperature={celsius}
+          onTemperatureChange={this.handleCelsiusChange} 
+        />
+        <TemperatureInput 
+          scale="f"
+          temperature={fahrenheit}
+          onTemperatureChange={this.handleFahrenheitChange}
+         />
+         <BoilingVerdict 
+          celsius={parseFloat(celsius)} />
+      </div>
+    );
+  }
+}
 
-//    render() {
-//      return (
-//        <form onSubmit={this.handleSubmit}>
-//          <label>
-//            name:
-//            <input type="text" value={this.state.value} onChange={this.hanleChange} />
-//          </label>
-//          <input type="submit" value="submit" />
-//        </form>
-//      );
-
-//    }
-//  }
-
-//  ReactDOM.render(
-//   <NameForm  />,
-//   document.getElementById('root')
-//   );
-
-// class Reservation extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       isGoing: true,
-//       numberOfGuests: 2
-//     };
-
-//     this.handleInputChange = this.handleInputChange.bind(this);
-//   }
-
-//   handleInputChange(event) {
-//     const target = event.target;
-//     const value = target.name === 'isGoing' ? target.checked : target.value;
-//     const name = target.name;
-//     this.setState({
-//       [name]: value
-//     });
-//   }
-  
-//   render() {
-//     return (
-//     <form>
-//         <label>
-//         Participate:
-//         <input 
-//           name="isGoing"
-//           type="checkbox"
-//           checked={this.state.isGoing}
-//           onChange={this.handleInputChange} />
-//       </label>
-//       <br />
-//       <label>
-//         Number of guests:
-//         <input 
-//           name="numberOfGuests"
-//           type="number"
-//           value={this.state.numberOfGuests}
-//           onChange={this.handleInputChange}
-//         />
-//       </label>
-//     </form>
-//     );
-//   }
-// }
-
-//  ReactDOM.render(
-//   <Reservation  />,
-//   document.getElementById('root')
-//   );
-
-ReactDOM.render(<input value="hi" />, document.getElementById('root'));
-
-setTimeout(function() {
-  ReactDOM.render(<input value={null} />, document.getElementById('root'));
-}, 1000);
+ReactDOM.render(
+  <Caculator />,
+  document.getElementById('root')
+);
