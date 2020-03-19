ad016e4b6ffd6e95beb9322976388e266e96924b
Heenjiang <12007881522@qq.com>
2020-03-19 23:27:10 +0300

    daily commit

--- src/index.js
+++ src/index.js
@@ -20,38 +20,136 @@ import './index.css';
 //   document.getElementById('root')
 // );
 
-function Blog(props) {
-  const sidebar = (
-    <ul>
-      {props.posts.map((post) => 
-      <li key={post.id}>
-        {post.title}
-      </li>)}
-    </ul>
-  );
+// function Blog(props) {
+//   const sidebar = (
+//     <ul>
+//       {props.posts.map((post) => 
+//       <li key={post.id}>
+//         {post.title}
+//       </li>)}
+//     </ul>
+//   );
 
-  const content = props.posts.map((post) => 
-      <div key={post.id}>
-        <h3>{post.title}</h3>
-      <p>{post.content}</p>
-      </div>
-  );
+//   const content = props.posts.map((post) => 
+//       <div key={post.id}>
+//         <h3>{post.title}</h3>
+//       <p>{post.content}</p>
+//       </div>
+//   );
 
-  return (
-    <div>
-      {sidebar}
-      <hr />
-      {content}
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       {sidebar}
+//       <hr />
+//       {content}
+//     </div>
+//   );
+// }
 
-const posts = [
-  {id: 1, title: 'Hello World', content: 'Welcome to learning React!'},
-  {id: 2, title: 'Installation', content: 'You can install React from npm.'}
-];
+// const posts = [
+//   {id: 1, title: 'Hello World', content: 'Welcome to learning React!'},
+//   {id: 2, title: 'Installation', content: 'You can install React from npm.'}
+// ];
 
-ReactDOM.render(
-  <Blog posts={posts} />,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <Blog posts={posts} />,
+//   document.getElementById('root')
+// );
+
+/**
+ * Form 
+ */
+
+//  class NameForm extends React.Component {
+//    constructor(props) {
+//      super(props);
+//      this.state = {value: ''};
+
+//      this.hanleChange = this.hanleChange.bind(this);
+//      this.handleSubmit = this.handleSubmit.bind(this);
+//    }
+
+//    hanleChange(event){
+//     this.setState({value: event.target.value});
+//    }
+
+//    handleSubmit(event) {
+//      alert('Submit name: ' + this.state.value);
+//      event.preventDefault();
+//    }
+
+//    render() {
+//      return (
+//        <form onSubmit={this.handleSubmit}>
+//          <label>
+//            name:
+//            <input type="text" value={this.state.value} onChange={this.hanleChange} />
+//          </label>
+//          <input type="submit" value="submit" />
+//        </form>
+//      );
+
+//    }
+//  }
+
+//  ReactDOM.render(
+//   <NameForm  />,
+//   document.getElementById('root')
+//   );
+
+// class Reservation extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       isGoing: true,
+//       numberOfGuests: 2
+//     };
+
+//     this.handleInputChange = this.handleInputChange.bind(this);
+//   }
+
+//   handleInputChange(event) {
+//     const target = event.target;
+//     const value = target.name === 'isGoing' ? target.checked : target.value;
+//     const name = target.name;
+//     this.setState({
+//       [name]: value
+//     });
+//   }
+  
+//   render() {
+//     return (
+//     <form>
+//         <label>
+//         Participate:
+//         <input 
+//           name="isGoing"
+//           type="checkbox"
+//           checked={this.state.isGoing}
+//           onChange={this.handleInputChange} />
+//       </label>
+//       <br />
+//       <label>
+//         Number of guests:
+//         <input 
+//           name="numberOfGuests"
+//           type="number"
+//           value={this.state.numberOfGuests}
+//           onChange={this.handleInputChange}
+//         />
+//       </label>
+//     </form>
+//     );
+//   }
+// }
+
+//  ReactDOM.render(
+//   <Reservation  />,
+//   document.getElementById('root')
+//   );
+
+ReactDOM.render(<input value="hi" />, document.getElementById('root'));
+
+setTimeout(function() {
+  ReactDOM.render(<input value={null} />, document.getElementById('root'));
+}, 1000);
