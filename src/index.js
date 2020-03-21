50d8152e406043a1f3d50e1b59211d22dd8d0768
Heenjiang <12007881522@qq.com>
2020-03-21 21:54:16 +0300

    react phlosipthy

--- src/index.js
+++ src/index.js
@@ -1,137 +1,113 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+/**如何利用react的思想，设计页面并实现 */
 
-// function FancyBorder(props) {
-//   return (
-//     <div className={'FancyBorder FancyBorder-' + props.color}>
-//       {props.children}
-//     </div>
-//   );
-// }
+/** 第一步：将设计好的UI划分为组件的层次 */
+/** 第二步：创建静态的版本（不考虑交互的功能） */
 
-// function WelcomeDialog() {
-//   return (
-//     <FancyBorder color="blue">
-//       <h1 className="Dialog-title">
-//         Welcome
-//       </h1>
-//       <p className="Dialog-message">
-//         Thank you for visiting our spacecraft!
-//       </p>
-//     </FancyBorder>
-//   );
-// }
-
-// function Contacts() {
-//   return <div className="Contacts" />;
-// }
-
-// function Chat() {
-//   return <div className="Chat" />;
-// }
-
-// function SplitPane(props) {
-//   return (
-//     <div className="SplitPane">
-//       <div className="SplitPane-left">
-//         {props.left}
-//       </div>
-//       <div className="SplitPane-right">
-//         {props.right}
-//       </div>
-//     </div>
-//   );
-// }
-
-// function App() {
-//   return (
-//     <SplitPane 
-//       left={
-//         <Contacts />
-//       }
-//       right={
-//         <Chat />
-//       }
-//     />
-//   );
-// }
-
-
-// }
-// function Dialog(props) {
-//   return (
-//     <FancyBorder color="blue">
-//       <h1 className="Dialog-title">
-//         {props.title}
-//       </h1>
-//       <p className="Dialog-message">
-//         {props.message}
-//       </p>
-//     </FancyBorder>
-//   );
-// }
-
-// function WelcomeDialog() {
-//   return (
-//     <Dialog 
-//       title="Welcome"
-//       message="Thank you for visiting our spacecraft!"
-//     />
-//   );
-// }
-function FancyBorder(props) {
-  return (
-    <div className={'FancyBorder FancyBorder-' + props.color}>
-      {props.children}
-    </div>
-  );
-}
-
-function Dialog(props) {
-  return (
-    <FancyBorder color="blue">
-      <h1 className="Dialog-title">
-        {props.title}
-      </h1>
-      <p className="Dialog-message">
-        {props.message}
-      </p>
-    </FancyBorder>
-  );
-}
-
-class SignUpDialog extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSignUp = this.handleSignUp.bind(this);
-    this.state = {login: ''};
-  }
-
+class ProductCategoryRow extends React.Component {
   render() {
+    const catefory = this.props.catefory;
     return (
-      <Dialog  title="Mars Exploration Program"
-               message="How should we refer to you?">
-        <input value={this.state.login}
-               onChange={this.handleChange} />
-        <button onClick={this.handleSignUp}>
-          Sign Me Up!
-        </button>
-      </Dialog>
+      <tr>
+        <th colSpan="2">
+          {catefory}
+        </th>
+      </tr>
     );
   }
+}
 
-  handleChange(e) {
-    this.setState({login: e.target.value})
-  }
+class ProductRow extends React.Component {
+  render() {
+    const product = this.props.product;
+    const name = product.stocked ?
+     product.name :
+     <span style={{color: 'red'}}>
+       {product.name}
+     </span>;
 
-  handleSignUp() {
-    alert(`Welcome aboard, ${this.state.login}!`);
+     return (
+       <tr>
+         <td>{name}</td>
+         <td>{product.price}</td>
+       </tr>
+     );
   }
 }
 
+class ProductTable extends React.Component {
+  render() {
+    const rows = [];
+    let lastCategory = null;
+
+    this.props.products.forEach(product => {
+      if(product.catefory !== lastCategory) {
+        rows.push(
+          <ProductCategoryRow
+            catefory={product.catefory}
+            key={product.catefory} />
+        );
+      }
+      rows.push(
+        <ProductRow
+          product={product}
+          key={product.name} />
+      );
+      lastCategory = product.catefory;
+    });
+
+    return (
+      <table>
+        <thread>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thread>
+        <tbody>{rows}</tbody>
+      </table>
+    );
+  }
+}
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <form>
+        <input type="text" placeholder="Search.." />
+        <p>
+          <input type="checkbox" />
+          {' '}
+          Only show products in stock
+        </p>
+      </form>
+    );
+  }
+}
+
+class FilterableProductTable extends React.Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <ProductTable products={this.props.products} />
+      </div>
+    );
+  }
+}
+
+const PRODUCTS = [
+  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+];
+
 ReactDOM.render(
-  <SignUpDialog   />,
+  <FilterableProductTable products={PRODUCTS} />,
   document.getElementById('root')
 );
